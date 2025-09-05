f94ddd8d8c93b1cf1e034b0e0e933996b182e971
eduar pacheco <eduar.pacheco@udea.edu.co>
2025-09-05 14:02:01 -0500

    actualizacion

--- src/frmjuego.java
+++ src/frmjuego.java
@@ -1,16 +1,21 @@
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.WindowConstants;
 
-public class frmjuego extends JFrame  {
+public class frmjuego extends JFrame {
 
     private JPanel pnlJugador1, pnlJugador2;
+    private jugador jugador1, jugador2;
+    private JTabbedPane tpJugadores;
 
-    public frmjuego(){
+    public frmjuego() {
 
         setSize(600, 300);
         setTitle("juego de cartas");
@@ -19,24 +24,113 @@ public class frmjuego extends JFrame  {
 
         JButton btnRepartir = new JButton("repartir");
         btnRepartir.setBounds(10, 10, 100, 25);
-        getContentPane().add(btnRepartir); 
+        getContentPane().add(btnRepartir);
 
         JButton btnVerificar = new JButton("verificar");
         btnVerificar.setBounds(120, 10, 100, 25);
-        getContentPane().add(btnVerificar); 
+        getContentPane().add(btnVerificar);
 
-        pnlJugador1=new JPanel();
+        // (NUEVO) Agregamos botones para escalaras y puntaje
+        JButton btnEscaleras = new JButton("Buscar Escaleras");
+        btnEscaleras.setBounds(230, 10, 150, 25);
+        getContentPane().add(btnEscaleras);
+
+        JButton btnPuntaje = new JButton("Calcular Puntaje");
+        btnPuntaje.setBounds(390, 10, 150, 25);
+        getContentPane().add(btnPuntaje);
+
+        pnlJugador1 = new JPanel();
+        pnlJugador1.setLayout(null);
         pnlJugador1.setBackground(new Color(153, 255, 50));
 
-        pnlJugador2=new JPanel();
+        pnlJugador2 = new JPanel();
+        pnlJugador2.setLayout(null);
         pnlJugador2.setBackground(new Color(0, 255, 255));
 
-        JTabbedPane tpJugadores = new JTabbedPane();
+        tpJugadores = new JTabbedPane();
         tpJugadores.addTab("martin estrada contreras", pnlJugador1);
-        tpJugadores.addTab("raul vidal", pnlJugador2); 
+        tpJugadores.addTab("raul vidal", pnlJugador2);
 
         tpJugadores.setBounds(10, 40, 550, 200);
-        getContentPane().add(tpJugadores); 
+        getContentPane().add(tpJugadores);
+
+        // agregar los eventos
+
+        btnRepartir.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                repartir();
+            }
+
+        });
+
+        btnVerificar.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                verificar();
+            }
+
+        });
+        jugador1 = new jugador();
+        jugador2 = new jugador();
+
+        // Eventos nuevos
+        btnEscaleras.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                switch (tpJugadores.getSelectedIndex()) {
+                    case 0:
+                        JOptionPane.showMessageDialog(null, jugador1.getEscaleras());
+                        break;
+                    case 1:
+                        JOptionPane.showMessageDialog(null, jugador2.getEscaleras());
+                        break;
+                }
+            }
+        });
+
+        btnPuntaje.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                switch (tpJugadores.getSelectedIndex()) {
+                    case 0:
+                        JOptionPane.showMessageDialog(null,
+                                "Puntaje de cartas sueltas: " + jugador1.getPuntajeCartasSueltas());
+                        break;
+                    case 1:
+                        JOptionPane.showMessageDialog(null,
+                                "Puntaje de cartas sueltas: " + jugador2.getPuntajeCartasSueltas());
+                        break;
+                }
+            }
+        });
+
+    }
+
+    private void repartir() {
+        // repartir cartas
+        jugador1.repartir();
+        jugador2.repartir();
+
+        // mostrar cartas de cada jugador
+        jugador1.mostrar(pnlJugador1);
+        jugador2.mostrar(pnlJugador2);
+
+    }
+
+    private void verificar() {
+        switch (tpJugadores.getSelectedIndex()) {
+            case 0:
+                JOptionPane.showMessageDialog(null, jugador1.getGrupos());
+
+                break;
+
+            case 1:
+                JOptionPane.showMessageDialog(null, jugador2.getGrupos());
+                break;
+        }
 
     }
 
---
+++ src/nombrecarta.java
@@ -0,0 +1,15 @@
+public enum nombrecarta {
+    AS,
+    DOS,
+    TRES,
+    CUATRO,
+    CINCO,
+    SEIS,
+    SIETE,
+    OCHO,
+    NUEVE,
+    DIEZ,
+    JACK,
+    QUEEN,
+    KING
+}
